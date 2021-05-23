ea09b191c283e41dc915c26253e03edafc52da43
Gustavo Villegas <79817045+opel777@users.noreply.github.com>
2021-05-23 01:55:54 -0300

    modal

---
+++ R3/app.js
@@ -0,0 +1,22 @@
+
+
+let cerrar= document.querySelectorAll(".close")[0];
+let abrir= document.querySelectorAll(".cta")[0];
+let modal= document.querySelectorAll("modal")[0];
+let modalC= document.querySelectorAll(".cont")[0];
+
+
+abrir.addEventListener("click",function(e) {
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("cont-close")
+});
+cerrar.addEventListener("click",function(){
+    modal.classList.toggle("contenido-close")
+    modalC..style.opacity="0";
+    modalC.style.visibility="hidden";
+    setTimeout(function(){
+
+    },1000)
+});
