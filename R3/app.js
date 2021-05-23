15f92ec6faf9e8bcc1207bd81b175070eeeeb8b3
Gustavo Villegas <79817045+opel777@users.noreply.github.com>
2021-05-23 02:14:34 -0300

    commit

--- R3/app.js
+++ R3/app.js
@@ -2,7 +2,7 @@
 
 let cerrar= document.querySelectorAll(".close")[0];
 let abrir= document.querySelectorAll(".cta")[0];
-let modal= document.querySelectorAll("modal")[0];
+let modal= document.querySelectorAll(".contenido-close")[0];
 let modalC= document.querySelectorAll(".cont")[0];
 
 
@@ -10,11 +10,11 @@ abrir.addEventListener("click",function(e) {
     e.preventDefault();
     modalC.style.opacity = "1";
     modalC.style.visibility = "visible";
-    modal.classList.toggle("cont-close")
+    modal.classList.toggle("contenido-close")
 });
 cerrar.addEventListener("click",function(){
-    modal.classList.toggle("contenido-close")
-    modalC..style.opacity="0";
+    modal.classList.toggle("cont-close")
+    modalC.style.opacity="0";
     modalC.style.visibility="hidden";
     setTimeout(function(){
 
